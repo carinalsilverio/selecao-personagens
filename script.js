35683380ee02aa442470b90aa4faa4188a2b7006
carinalsilverio <carinalsilverio@gmail.com>
2022-06-30 20:04:11 -0300

    Alteração caminho imagens

--- script.js
+++ script.js
@@ -12,14 +12,13 @@
     - alterar o nome do jogador 1
 */
 
-const personagens = document.querySelectorAll(".personagem");
+const personagens = document.querySelectorAll('.personagem');
 
 personagens.forEach((personagem) => {
-  personagem.addEventListener("mouseenter", () => {
-
+  personagem.addEventListener('mouseenter', () => {
     const idSelecionado = personagem.attributes.id.value;
 
-    if(idSelecionado === 'ultron') return;
+    if (idSelecionado === 'ultron') return;
 
     /* 
         OBJETIVO 1 - quando passar o mouse em cima do personagem temos que:
@@ -28,10 +27,10 @@ personagens.forEach((personagem) => {
 
         - retirar a classe selecionado do personagem que está selecionado
     */
-    const personagemSelecionado = document.querySelector(".selecionado");
-    personagemSelecionado.classList.remove("selecionado");
+    const personagemSelecionado = document.querySelector('.selecionado');
+    personagemSelecionado.classList.remove('selecionado');
 
-    personagem.classList.add("selecionado");
+    personagem.classList.add('selecionado');
 
     /* 
         OBJETIVO 2 - quando passar o mouse em cima do personagem da listagem, trocar a imagem e nome do personagem grande
@@ -40,11 +39,11 @@ personagens.forEach((personagem) => {
     */
 
     const imagemJogador1 = document.getElementById('personagem-jogador-1');
-    imagemJogador1.src = `./img/${idSelecionado}.png`;
+    imagemJogador1.src = `img/${idSelecionado}.png`;
 
     const nomeJogador1 = document.getElementById('nome-jogador-1');
     const nomeSelecionado = personagem.getAttribute('data-name');
-    
+
     nomeJogador1.innerHTML = nomeSelecionado;
   });
 });
