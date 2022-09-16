096d0a25d243141fb9a6b96530543ba38edc88d9
carinalsilverio <carinalsilverio@gmail.com>
2022-09-15 22:06:31 -0300

    Atualização README

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# selecao-personagens
+# Seletor de Personagens
+>Programa Dev em Dobro
+
+
+![preview](/github/preview.png)
+
+Projeto criado durante programa da plataforma Dev em Dobro.
+
+🔗[Clique aqui para acessar](https://carinalsilverio.github.io/selecao-personagens/)
+
+
+## 🛠️ Tecnologias
+- HTML
+- CSS
+- JavaScript
+
+
+## 📧 Contato
+carinalsilverio@gmail.com
--- script.js
+++ script.js
@@ -1,17 +1,3 @@
-/* 
- OBJETIVO 1 - quando passar o mouse em cima do personagem temos que:
-
-  - colocar a classe selecionado no personagem que passamos o mouse em cima para adicionar a animação nele 
-
- - retirar a classe selecionado do personagem que está selecionado
-
-
- OBJETIVO 2 - quando passar o mouse em cima do personagem, trocar a imagem e nome do personagem grande
-
-    - alterar a imagem do jogador 1
-    - alterar o nome do jogador 1
-*/
-
 const personagens = document.querySelectorAll('.personagem');
 
 personagens.forEach((personagem) => {
